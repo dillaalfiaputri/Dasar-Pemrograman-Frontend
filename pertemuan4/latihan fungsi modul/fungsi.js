851c8d9aefad6d23a8fe9e6e65dd17579be22b1e
via <dillaalfiaputri@gmail.com>
2025-10-08 18:53:44 +0700

    Add files via upload

---
+++ pertemuan4/latihan fungsi modul/fungsi.js
@@ -0,0 +1,38 @@
+var dataBarang = [
+    { nama: "Buku Tulis", harga: 5000 },
+    { nama: "Pensil", harga: 2000 },
+    { nama: "Spidol", harga: 8000 }
+];
+
+
+function showBarang(){
+    var listBarang = document.getElementById("list-barang");
+    listBarang.innerHTML = "";
+
+    for (let i = 0; i < dataBarang.length; i++) {
+        var btnEdit = "<a href='#' onclick='editBarang(" + i + ")'>Edit</a>";
+        var btnHapus = "<a href='#' onclick='deleteBarang(" + i + ")'>Hapus</a>";
+
+        listBarang.innerHTML +=
+            "<li>" + dataBarang[i].nama + " - Rp" + dataBarang[i].harga +
+            " [" + btnEdit + " | " + btnHapus + "]</li>";
+    }
+}
+
+
+function addBarang(){
+    var input = document.querySelector("input[name=barang]");
+    dataBarang.push(input.value);
+    showBarang();
+}
+function editBarang(id){
+    var newBarang = prompt("Nama baru", dataBarang[id]);
+    dataBarang[id] = newBarang;
+    showBarang();
+}
+function deleteBarang(id){
+    dataBarang.splice(id, 1);
+    showBarang();
+}
+س
+showBarang();
---
+++ pertemuan4/modules/lingkaran.js
@@ -0,0 +1,5 @@
+import { PHI } from './math-constants.js';
+
+export const luasLingkaran = (radius) => PHI * radius ** 2;
+
+export const kelilingLingkaran = (radius) => 2 * PHI * radius;
